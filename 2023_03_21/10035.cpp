467870e601da3a7be9a67df8f2025a4ed400732f
YeeGod <72200322+YeeGod@users.noreply.github.com>
2023-05-09 18:58:02 +0800

    Update 10035.cpp

--- 2023_03_21/10035.cpp
+++ 2023_03_21/10035.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
-
-
-}
+	long long a, b;
+	while((cin >> a >> b) && (a+b > 0)) {
+		// makes a > b
+		if(a < b) swap(a, b);
+		
+		int cnt = 0, t = 0;
+		
+		while(b) {
+			int sa = a%10;
+			int sb = b%10;
+			a /= 10;
+			b /= 10;
+			if(sa+sb+t >= 10) {
+				t = 1;
+				cnt++;
+			} else 
+				t = 0;
+		}
+		while(a) {
+			int sa = a%10;
+			a /= 10;
+			if(sa+t >= 10) {
+				t = 1;
+				cnt++;
+			} else 
+				break;
+		}
+		if(cnt == 0) cout << "No carry operation.\n";
+		else if(cnt == 1) cout << "1 carry operation.\n";
+		else cout << cnt << " carry operations.\n";
+	}
+} 
