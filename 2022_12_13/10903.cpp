4915e2d69725d4bd5d3ada4aac27bcc6954cecb6
YeeGod <72200322+YeeGod@users.noreply.github.com>
2023-05-20 23:23:14 +0800

    Update 10903.cpp

--- 2022_12_13/10903.cpp
+++ 2022_12_13/10903.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	int num;
+	while((cin >> num) && num) {
+		string ans;
+		int cnt = 0;
+		while(num) {
+			cnt += num % 2;
+			ans += '0'+(num%2); 
+			num >>= 1;
+		}
+		reverse(ans.begin(), ans.end());
+		cout << "The parity of " << ans << " is " << cnt << " (mod 2).\n";
+	}
+}
