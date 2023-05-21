7cdc8c1e6fe6cd192cc1af30c4620e7106ba9a67
YeeGod <72200322+YeeGod@users.noreply.github.com>
2023-05-21 11:35:41 +0800

    Update 11584.cpp

--- 2022_12_13/11584.cpp
+++ 2022_12_13/11584.cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+/*
+加了 dp : AC 13 ms, 1.3MB
+沒加 dp : AC 20 ms, 320KB
+*/
+
+string line;
+bool dp[1005][1005];
+int len[1005];
+
+bool isPalidrome(int l, int r) {
+	int ll = l, rr = r;
+	while(l < r) {
+		if(line[l] != line[r]) return false;
+		l++;
+		r--;
+		if(dp[l][r]) return true;
+	}
+	dp[ll][rr] = true;
+	return true;
+}
+
+
+int main() {
+	int m;
+	cin >> m;
+	while(m--) {
+		cin >> line;
+		memset(dp, false, sizeof(dp));
+		memset(len, 0x7f, sizeof(len));
+		for(int i = 0; i < line.size(); i++) {
+			for(int j = 0; j <= i; j++) {
+				if(isPalidrome(j, i)) {
+					if(len[i] > len[j-1]+1)	len[i] = len[j-1]+1;
+				}
+			}
+		}
+		cout << len[line.size()-1] << endl;
+	} 
+}
